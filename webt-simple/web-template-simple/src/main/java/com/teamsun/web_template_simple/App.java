95f4e2e29a31697db5432b92df72b0b73771e4b1
simplty <simplty@gmail.com>
2016-10-13 15:39:02 +0800

    first commit

---
+++ webt-simple/web-template-simple/src/main/java/com/teamsun/web_template_simple/App.java
@@ -0,0 +1,13 @@
+package com.teamsun.web_template_simple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
